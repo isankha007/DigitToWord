26b7a0a9957a225d2e9a5effd7ad49476724b1d9
sankhadeep <sankhadeep2007@gmail.com>
2019-03-02 10:30:30 +0530

    logical issue resolved

--- DigitToword.c
+++ DigitToword.c
@@ -3,67 +3,84 @@
 
 int main(){
 
-	const char *strings[] = {"zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen"
+	const char *strings[] = {"zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen",
 			"eighteen","nineteen","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred","thousand"};
-	int number=310,digit;
+	int number=310,digit,n=1000;
 	printf("Enter the number ==\n");
-	scanf("%d",&number);
+	scanf("%d",&n);
+
+	//scanf("%d",&number);
+
 	int sum=0;
-	int temp=number,length=0,oldlenght=0;
-	while(temp>0){
-		temp=temp/10;
-		length++;
-	}
-	oldlenght=length;
-	while(length>0){
-		//       printf("%d\n",length);
-		switch (length){
-
-		case 1:
-			if(number>0)
-				printf("%s\n",strings[number]);
-
-			break;
-		case 2:
-			if(number>0){
-				digit=number/10;
-				//			printf("digit deci== %d\n",digit);
-				if(digit>1){
-					sum+=strlen(strings[digit-1])+strlen(strings[digit+18]);
-					printf("%s",strings[digit-1+18]);
-					;
-				}else{
-                    if(number%10>0){
-                    	printf(" %s\n",strings[digit-1+(number%10)+10]);
-                    }else{
-                    	printf("%s\n",strings[digit-1+10]);
-                    }
-
-					length=0;
-				}
-				number=number%10;
-			}
-			break;
-		case 3:
-			digit=number/100;
-			//			printf("digit +++ %d\n",digit);
-			sum+=strlen("hundred")+strlen(strings[digit]);
-			printf("%s hundred ",strings[digit]);
-			number=number%100;
-			if(number>0)
-			  printf(" and ");
-
-			break;
-		case 4:
-			digit=number/1000;
-			//			printf("digit== %d\n",digit);
-			sum+=strlen("thousand")+strlen(strings[digit]);
-			printf("%s thousand ",strings[digit]);
-			number=number%1000;
-			break;
+//	while(n>0){
+		number=n;
+		printf("NUMBER=%d\n",number);
+		int temp=number,length=0,oldlenght=0;
+		while(temp>0){
+			temp=temp/10;
+			length++;
 		}
-		length--;
-	}
-	printf("\n");
+		oldlenght=length;
+		while(length>0){
+			// printf("%d\n",length);
+			switch (length){
+
+			case 1:
+				if(number>0){
+					printf("%s===\n",strings[number]);
+					sum+=strlen(strings[number]);
+				}
+				break;
+			case 2:
+				if(number>0){
+					digit=number/10;
+					//printf("digit deci== %d\n",digit);
+					if(digit>1){
+						sum+=strlen(strings[digit-1+19]);   //strlen(strings[digit-1])+strlen(strings[digit+18]);
+						printf("_--- %s",strings[digit-1+19]);
+					}else{
+						if(number%10>0){
+							printf("==++ %s++=\n",strings[digit-1+(number%10)+10]);
+							sum+=strlen(strings[digit-1+(number%10)+10]);
+						}else{
+							printf("++ == %s===\n",strings[digit-1+10]);
+							sum+=strlen(strings[digit-1+10]);
+						}
+
+						length=0;
+					}
+					number=number%10;
+				}
+				break;
+			case 3:
+				if(number>0){
+					digit=number/100;
+					//			printf("digit +++ %d\n",digit);
+					sum+=strlen("hundred")+strlen(strings[digit]);
+					printf("%s hundred ",strings[digit]);
+					number=number%100;
+					if(number>0){
+						printf("and ");
+						sum+=3;
+					}
+				}
+
+				break;
+			case 4:
+				if(number>0){
+					digit=number/1000;
+					//			printf("digit== %d\n",digit);
+					sum+=strlen("thousand")+strlen(strings[digit]);
+					printf("%s thousand ",strings[digit]);
+					number=number%1000;
+				}
+				break;
+			}
+			length--;
+		}
+		printf("======================\n");
+		n--;
+//	}
+	printf("SUM %d \n",sum);
 	//	printf("%s,%d ,%d\n",strings[15],length,digit);
 }
